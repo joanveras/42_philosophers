52111d66dc03186c0cb60dbae85b17d18e17315e
Joan Veras <jveras@c2r9s3.42.rio>
2024-08-03 08:31:06 -0300

    now handle the problem when 0(zero) is passed as num_of_philos

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: jveras <jveras@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 09:13:51 by jveras            #+#    #+#             */
-/*   Updated: 2024/07/13 13:17:40 by jveras           ###   ########.fr       */
+/*   Updated: 2024/08/03 07:57:42 by jveras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ int	main(int argc, char **argv)
 	if (arguments_err(argc, argv))
 		return (1);
 	init_data(&data, argv);
-	if (handle_bad_cases(data))
+	if (data->num_of_philos == 0)
+		return (1);
+	else if (handle_bad_cases(data))
 		return (1);
 	init_forks(&data);
 	init_philos(&data);
